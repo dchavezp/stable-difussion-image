b29ee23b947d5c8e6f6b69dd1f11c26564257f59
DEWITT SCOTT CHAVEZ PONCE <dschavezp@gmail.com>
2023-05-08 13:02:41 -0500

    updating upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         <title>Stable Diffusion Image Generator</title>
         <meta name="description" content="Devvitt ai tools" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main className="grid place-content-center h-screen font-sans">
         <section className="flex flex-col gap-4 items-center w-fit  bg-neutral-focus rounded-md py-10 px-10">
@@ -82,7 +82,8 @@ export default function Home() {
                 />
                 <ResolutionPicker value={resolution} setState={setResolution} />
                 <Link
-                  download
+                  referrerPolicy="no-referrer"
+                  target="_blank"
                   href={`/api/download/?imageId=${id}&resolution=${resolution}`}
                   className="btn btn-secondary rounded-none w-full"
                 >
@@ -92,6 +93,18 @@ export default function Home() {
             ) : (
               <></>
             )}
+          </div>
+          <div>
+            Made by{" "}
+            <a
+              className="underline decoration-1"
+              target="_blank"
+              rel="noreferrer"
+              href="https://devvitt-site.vercel.app/"
+              aria-label="Devvitt's porfolio"
+            >
+              Devvitt
+            </a>
           </div>
         </section>
       </main>
