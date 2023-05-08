d0fed0a6bda6c76f22e263458d2ce6e25bd763c0
DEWITT SCOTT CHAVEZ PONCE <dschavezp@gmail.com>
2023-05-08 13:09:05 -0500

    updating download

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest,
     res: NextApiResponse) {
     const { imageId, resolution } = req.query;
-    res.redirect(`http://res.cloudinary.com/devvitt/image/upload/fl_attachment/c_scale,f_png,w_${resolution}/${imageId}`);
+    res.redirect(`http://res.cloudinary.com/devvitt/image/upload/c_scale,f_png,w_${resolution}/${imageId}`);
 }
 
 export const config = {
